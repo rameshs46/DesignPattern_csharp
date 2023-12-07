fb6a58eec4a10ab793ccdb8df6696f2be52c2bbc
Ramesh Sengamalai <rsengamalai@gmail.com>
2023-12-07 08:35:02 +0800

    SingleTon pattern knowlegde revision.

---
+++ DesignPattern_Csharp/ClsSingleTon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPattern_Csharp
+{
+	public class ClsSingleTon
+	{
+		public static int intCounter;
+		private ClsSingleTon()
+		{
+			// This is a private constructor
+		}
+		public static void Hit()
+		{
+			intCounter++;
+		}
+		public static int getTotalHits()
+		{
+			return intCounter;
+		}
+	}
+}
+
